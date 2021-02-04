3e63e17637f264c5308eb2c506dc7921ffbdc0b3
mehmehmehlol <hkmeganlo@gmail.com>
2021-02-03 23:50:10 -0800

    commit tree 71fcf14b62ce2411aa3493e98de51fc0d1cd4b1a

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function findMinAndRemoveSorted(arr) {
+    return arr.shift();
+}
 
+function merge(arr1, arr2) {
+    let sorted = [];
+    while (arr1.length !== 0 && arr2.length !== 0) {
+        if (arr1[0] < arr2[0]) {
+            sorted.push(findMinAndRemoveSorted(arr1));
+        } else {
+            sorted.push(findMinAndRemoveSorted(arr2));
+        }
+    }
+    return sorted.concat(arr1).concat(arr2);
+}
+
+function mergeSort(arr) {
+    let mid = Math.floor(arr.length / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid, arr.length);
+    let sorted;
+
+    if (arr.length <= 1) return arr;
+    else sorted = merge(mergeSort(left), mergeSort(right))
+
+    return sorted;
+}
